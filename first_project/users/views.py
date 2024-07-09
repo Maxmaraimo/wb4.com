0730714a549f13f7f1570c33614609353696b1df
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-09 13:25:11 +0500

    update views.py

--- first_project/users/views.py
+++ first_project/users/views.py
@@ -56,3 +56,279 @@ def profile(request):
         'p_form': ProfileForm(instance=request.user.profile)
     }
     return render(request, 'profile.html', context)
+
+# 
+
+
+def wb4pay(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/wb4pay.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/wb4pay.html', context)
+
+
+
+def video(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/video.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/video.html', context)
+
+def support(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/support.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/support.html', context)
+
+def stickers(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/stickers.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/stickers.html', context)
+
+def services(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/services.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/services.html', context)
+
+def photos(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/photos.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/photos.html', context)
+
+def news(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/news.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/news.html', context)
+
+def music(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/music.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/music.html', context)
+
+def message(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/message.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/message.html', context)
+
+def markett(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/markett.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/markett.html', context)
+
+def groups(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/groups.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/groups.html', context)
+
+def game(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/game.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/game.html', context)
+
+def clips(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/clips.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/clips.html', context)
+
+def calls(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/calls.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/calls.html', context)
+
+def friends(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'templatesField/friends.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'templatesField/friends.html', context)
+
